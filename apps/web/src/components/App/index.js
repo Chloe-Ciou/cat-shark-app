e16654af2fa37f089fbdcd2c664a2169889f033c
chloeciou78 <chloe.ciou@telus.com>
2022-06-26 04:36:07 -0400

    Feat(Fronend): set up & build fronend

---
+++ apps/web/src/components/App/index.js
@@ -0,0 +1,18 @@
+import '../../styles/App.scss';
+import Loading from '../Spinner';
+import Carousel from '../Carousel';
+import APIProvider from '../../contexts/APIProvider';
+import LoadingProvider from '../../contexts/LoadingProvider';
+
+function App() {
+  return (
+    <LoadingProvider>
+      <APIProvider>
+        <Carousel />
+        <Loading />
+      </APIProvider>
+    </LoadingProvider>
+  );
+}
+
+export default App;
---
+++ apps/web/src/hooks/useImages.js
@@ -0,0 +1,29 @@
+import { useState, useCallback } from "react";
+
+import { useAPI } from "../contexts/APIProvider";
+import { useLoading } from "../contexts/LoadingProvider";
+
+export const useImages = () => {
+  const api = useAPI();
+  const { setIsLoading } = useLoading();
+  const [images, setImages] = useState([]);
+
+  const getImages = useCallback((types, cb) => {
+    setIsLoading(true);
+
+    api.get(`/images?types=${types.join(',')}`)
+      .then((data) => {
+        setImages(data.data);
+      }).catch((_) => {
+        setImages([]);
+      }).finally(() => {
+        cb?.();
+        setIsLoading(false);
+      });
+  }, [api, setIsLoading]);
+
+  return {
+    images,
+    getImages,
+  }
+};
